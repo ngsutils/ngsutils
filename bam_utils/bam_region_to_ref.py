ebbaec8603a30cd4c31bdcf74267053554dff213
Marcus Breese <mbreese@iupui.edu>
2011-01-18 16:49:47 -0500

    docs

--- bam_utils/bam_region_to_ref.py
+++ bam_utils/bam_region_to_ref.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python
 """
 This takes a BAM file that has been mapped to a genomic region and converts 
-the mapping to genomic coordinates.
+the mapping to genomic coordinates.  This can be used to convert reads mapped
+against a junction library or targetted resequencing back to genomic 
+coordinates.  The names of the reference sequences should be named:
+chrom:start-end (0-based start).  If there are gaps (junctions), they should
+be named: chrom:start-end,start-end,etc...
 
-For example: 
+In this is the case, this script will ensure the proper conversion of 
+reference, start position, and CIGAR alignment.
+
+Example 1: 
 chr1:1000-2000    20    50M
 
-is converted to:
+converted to:
 chr1    1020    50M
+
+Example 2:
+chr1:1000-1050,2000-2050,3000-4000    25    100M
+
+converted to:
+chr1    1025    25M950N50M950M25M
+
 """
 
 import os,sys,gzip
-sys.path.append(os.path.join(os.path.dirname(__file__),"..","utils")) #eta
-sys.path.append(os.path.join(os.path.dirname(__file__),"..","ext")) #pysam
-
-from eta import ETA
+from support.eta import ETA
 import pysam
 
 def usage():
