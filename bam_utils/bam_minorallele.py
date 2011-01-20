abc03a96537271e3e38661035a542b9ae707c662
Marcus Breese <mbreese@iupui.edu>
2011-01-20 15:56:10 -0500

    Moved to 1-based positions for bases

--- bam_utils/bam_minorallele.py
+++ bam_utils/bam_minorallele.py
@@ -108,7 +108,7 @@ def bam_minorallele(bam_fname,ref_fname,min_qual=0, min_count=0, num_alleles = 0
             refback = refcount-background
             altback = altcount-background
 
-            cols = [chrom,pileup.pos,refbase,altbase,total,refcount,altcount,background,refback,altback]
+            cols = [chrom,(pileup.pos+1),refbase,altbase,total,refcount,altcount,background,refback,altback]
             if robjects and num_alleles:
                 ci_low,ci_high = calc_cp_ci(refback+altback,altback,num_alleles)
                 cols.append(float(altback) / (refback+altback))
