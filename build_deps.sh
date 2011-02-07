693c04adc4043234ca1bda308135008cca82c388
Marcus Breese <mbreese@iupui.edu>
2011-02-07 05:45:14 -0500

    modified builddeps to log to a file; wrappers now quit if build_deps error"

--- build_deps.sh
+++ build_deps.sh
@@ -12,6 +12,11 @@ usage(){
     exit 1
 }
 
+control_c() {
+    rm -rf `dirname $0`/ext/*
+    exit 1
+}
+
 if [ "$1" == "-h" ]; then
     usage
 fi
@@ -20,28 +25,36 @@ if [ -e "`dirname $0`/ext/pysam" ]; then
     exit
 fi
 
-echo "Downloading and building dependencies..." >&2
+trap control_c SIGINT
+
+echo "[depends] Downloading and building dependencies..." >&2
 
 WORK=`dirname $0`/ext/work
-
 mkdir -p $WORK
 cd $WORK
 rm -rf *
 
 python -c "import Cython" > /dev/null
 if [ $? -ne 0 ]; then
+    echo "[cython] downloading" >&2
     curl -sLO "http://pypi.python.org/packages/source/C/Cython/Cython-0.14.1.tar.gz"
     tar zxf Cython-0.14.1.tar.gz
     cd Cython-0.14.1
-    python setup.py build >> $WORK/build.log
-    python setup.py install --user >> $WORK/build.log
+    echo "[cython] building" >&2
+    python setup.py build &>> $WORK/build.log
+    python setup.py install --user &>> $WORK/build.log
     cd ..
 fi
 
+echo "[pysam] downloading" >&2
 curl -sLO "http://pysam.googlecode.com/files/pysam-0.3.1.tar.gz"
 tar zxf pysam-0.3.1.tar.gz
 cd pysam-0.3.1
-python setup.py build >> $WORK/build.log
+echo "[pysam] building" >&2
+python setup.py build &>> $WORK/build.log
 
 cp -r build/lib*/pysam ../..
 cp build/lib*/*.so ../../pysam
+echo "Done!" >&2
+echo "" >&2
+
