14ec0f8e8166d0b77dda7135d04089226cfdab0e
Marcus Breese <mbreese@iupui.edu>
2011-02-14 13:53:24 -0500

    moved drivers to common.sh

---
+++ common.sh
@@ -0,0 +1,38 @@
+if [ "$1" == "" ]; then
+    usage
+fi
+
+"$DIR"/build_deps.sh
+if [ $? -ne 0 ]; then exit; fi
+
+export PYTHONPATH=$PYTHONPATH:"$DIR":"$DIR"/ext
+
+if [ "$1" == "help" ]; then
+    if [ "$2" == "" ]; then
+        usage
+    fi
+    
+    action=$PREFIX$2.py
+    
+    if [ ! -e "$DIR"/$SUBDIR/$action ]; then
+        action=$PREFIX$2.sh
+        if [ ! -e "$DIR"/$SUBDIR/$action ]; then
+            echo "Unknown command '$2'"
+            exit 1
+        fi
+    fi
+    "$DIR"/$SUBDIR/$action -h
+else
+    action=$PREFIX$1.py
+    
+    if [ ! -e "$DIR"/$SUBDIR/$action ]; then
+        action=$PREFIX$1.sh
+        if [ ! -e "$DIR"/$SUBDIR/$action ]; then
+            echo "Unknown command '$1'"
+            exit 1
+        fi
+    fi
+    shift
+
+    "$DIR"/$SUBDIR/$action $@
+fi
