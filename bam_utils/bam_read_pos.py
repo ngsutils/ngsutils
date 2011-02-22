ef9fece9ec6e8acdf627667216c81841603ced9a
Marcus Breese <mbreese@iupui.edu>
2011-02-22 11:56:05 -0500

    Repurpose script to show the position of a read in a bam file.  Useful when you can't rely on ETA.

--- bam_utils/bam_read_pos.py
+++ bam_utils/bam_read_pos.py
@@ -1,44 +1,31 @@
 #!/usr/bin/env python
 
-import sys,gzip,os
-from support.eta import ETA
+import sys,os
 import pysam
 
-bam_cigar = ['M','I','D','N','S','H','P']
-
-def bam_read_pos(fname,show_all=False):
+def bam_read_pos(fname,readname):
     bamfile = pysam.Samfile(fname,"rb")
-    eta = ETA(0,bamfile=bamfile)
-
-    for read in bamfile.fetch():
-        eta.print_status(extra=read.qname,bam_pos=(read.rname,read.pos))
-        if not read.is_unmapped:
-            print '%s\t%s\t%s\t%s' % (read.qname, bamfile.getrname(read.rname), read.pos, ''.join(['%s%s' % (length,bam_cigar[op]) for op,length in read.cigar]))
-        elif show_all:
-            print '%s\t*\t0\t\n' % (read.qname)
+    count = 0
+    pos = 0
     
-    eta.done()
+    for read in bamfile:
+        if read.qname == readname:
+            pos = count
+        count += 1
+        
     bamfile.close()
+    print "%s position: %s of %s" % (readname,pos,count)
 
 def usage():
     print """\
-Usage: %s bamfile
-
-Ouputs the read positions of all mapped reads in a tab-delimited format.
-
-Format:
-name    ref    pos    cigar
-
+Usage: %s bamfile read_name
 
 """ % os.path.basename(sys.argv[0])
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or not os.path.exists(sys.argv[-1]):
+    if len(sys.argv) != 3 or not os.path.exists(sys.argv[1]):
         usage()
         sys.exit(1)
 
-    if sys.argv[1] == '-all':
-        bam_read_pos(sys.argv[2],True)
-    else:
-        bam_read_pos(sys.argv[1])
+    bam_read_pos(sys.argv[1], sys.argv[2])
 
