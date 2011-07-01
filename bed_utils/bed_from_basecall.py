1017a237876519d6a7e9bbe06402162060d83b70
Marcus Breese <mbreese@iupui.edu>
2011-07-01 17:08:58 -0400

    added support for stdin

--- bed_utils/bed_from_basecall.py
+++ bed_utils/bed_from_basecall.py
@@ -5,18 +5,23 @@ Converts a file in basecall format to BED3 format.
 import sys,os
 
 def basecall_to_bed(fname):
-    with open(fname) as f:
-        for line in f:
-            if line[0] == '#':
-                continue
-            cols = line.strip('\n').split('\t')
-            chrom = cols[0]
-            pos = int(cols[1])-1
-            sys.stdout.write('%s\t%s\t%s\n' % (chrom,pos,pos+1,))
+    if fname == '-':
+        f = sys.stdin
+    else:
+        f = open(fname)
+    for line in f:
+        if line[0] == '#':
+            continue
+        cols = line.strip('\n').split('\t')
+        chrom = cols[0]
+        pos = int(cols[1])-1
+        sys.stdout.write('%s\t%s\t%s\n' % (chrom,pos,pos+1,))
+    if fname != '-':
+        f.close()
 
 def usage():
     print __doc__
-    print 'Usage: %s basecall.txt' % os.path.basename(sys.argv[0])
+    print 'Usage: %s basecall.txt (- for stdin)' % os.path.basename(sys.argv[0])
     sys.exit(1)
 if __name__ == '__main__':
     fname = None
