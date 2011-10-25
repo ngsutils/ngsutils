f9d3279ac450c61859d706dd272d52a8ebb87eb5
Marcus Breese <mbreese@iupui.edu>
2011-10-25 10:58:40 -0700

    Added script to verify a BAM file isn't corrupt (using samtools view piped to /dev/null)

---
+++ bam_utils/bam_check.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Usage: bamutils check filename.bam"
+    exit 1
+fi
+
+if [ "`which samtools`" == "" ]; then
+    echo 'Missing samtools from $PATH'
+    exit 1
+fi
+echo -n "$1 "
+samtools view "$1" &> /dev/null
+if [ $? == 0 ]; then
+    echo "OK"
+    exit 0
+else
+    echo "ERROR"
+    exit 1
+fi
