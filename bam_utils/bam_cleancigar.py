dee4eebacd18d1d9615960382916cf1ce3998b09
Marcus Breese <mbreese@iupui.edu>
2012-01-18 22:25:49 -0800

    updated cleancigar to work and be a little cleaner

--- bam_utils/bam_cleancigar.py
+++ bam_utils/bam_cleancigar.py
@@ -9,46 +9,61 @@ import pysam
 def bam_cleancigar(infile, outfile):
     bam = pysam.Samfile(infile,"rb")
     out = pysam.Samfile(outfile,"wb", template=bam)
+    total = 0
+    count = 0
     for read in bam:
         newcigar = []
         changed = False
-        for op, length in read.cigar:
-            if length > 0:
-                newcigar.append((op,length))
-            else:
-                changed = True
+        if not read.is_unmapped:
+            for op, length in read.cigar:
+                if length > 0:
+                    newcigar.append((op,length))
+                else:
+                    changed = True
         
-        if changed:
-            read.cigar = newcigar
-        
+            if changed:
+                read.cigar = newcigar
+                count += 1
+        total += 1
         out.write(read)
         
     bam.close()
     out.close()
-    
+    sys.stderr.write('Wrote: %s reads\nAltered: %s\n' % (total,count))
     
 
 def usage():
     print __doc__
-    print "Usage: bamutils cleancigar inbamfile outbamfile"
+    print """Usage: bamutils cleancigar {-f} inbamfile outbamfile
+
+Options:
+  -f     Force overwriting an existing outfile
+"""
     sys.exit(-1)
 
 if __name__ == "__main__":
     infile = None
     outfile = None
+    force = False
     
     for arg in sys.argv[1:]:
         if arg == "-h":
             usage()
-        elif os.path.exists(arg):
-            if not infile:
-                infile = arg
-            elif not outfile:
+        elif arg == "-f":
+            force = True
+        elif not infile:
+            if os.path.exists(os.path.expanduser(arg)):
+                infile = os.path.expanduser(arg)
+            else:
+                sys.stderr.write("File: %s not found!" % arg)
+                usage()
+        elif not outfile:
+            if force or not os.path.exists(os.path.expanduser(arg)):
                 outfile = arg
             else:
+                sys.stderr.write("File: %s exists! Not overwriting without -f force." % arg)
                 usage()
         else:
-            sys.stderr.write("File: %s not found!\n" % arg)
             usage()
             
     if not infile or not outfile:
