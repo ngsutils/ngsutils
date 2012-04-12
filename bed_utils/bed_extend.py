3e7d245570035cc819c5ed7153448bf82ba501a5
Marcus Breese <mbreese@iupui.edu>
2012-04-11 22:43:11 -0700

    pep8

--- bed_utils/bed_extend.py
+++ bed_utils/bed_extend.py
@@ -3,28 +3,31 @@
 Extends BED regions
 '''
 
-import os,sys
+import os
+import sys
 import support.ngs_utils
+
 
 def usage():
     print __doc__
     print """\
 Usage: bedutils extend {+}SIZE bedfile
 
-SIZE is what the total size of the region should be.  The size of the region 
+SIZE is what the total size of the region should be.  The size of the region
 will be extended or reduced to make the total length exactly SIZE. The region
 is always adjusted at the 3' end, respective to the given strand.
 
-If the first character of SIZE is '+', then the region is extended 
+If the first character of SIZE is '+', then the region is extended
 SIZE bases, regardless of how long it is to start with.
 
 """
     sys.exit(1)
 
-def bed_extend(fname,size,relative=False):
+
+def bed_extend(fname, size, relative=False):
     with support.ngs_utils.gzip_opener(fname) as f:
         for line in f:
-            chrom,start,end,name,score,strand = line.strip().split('\t')
+            chrom, start, end, name, score, strand = line.strip().split('\t')
 
             if strand == '+':
                 if relative:
@@ -36,18 +39,18 @@ def bed_extend(fname,size,relative=False):
                     start = int(start) - size
                 else:
                     start = int(end) - size
-        
+
             if start < 0:
                 start = 0
-        
-            sys.stdout.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (chrom,start,end,name,score,strand))
+
+            sys.stdout.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (chrom, start, end, name, score, strand))
 
 if __name__ == '__main__':
     fname = None
     size = None
     relative = False
     last = None
-    
+
     for arg in sys.argv[1:]:
         if arg == '-h':
             usage()
@@ -62,8 +65,8 @@ if __name__ == '__main__':
         else:
             print "Unknown option: %s" % arg
             usage()
-    
+
     if not fname or not size:
         usage()
-    
-    bed_extend(fname,size,relative)
+
+    bed_extend(fname, size, relative)
