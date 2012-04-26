c0867ff7f2d6ad4b92d099c108e92b33dfb3bf6e
Marcus Breese <mbreese@iupui.edu>
2012-04-25 23:07:01 -0400

    updated init.sh order

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-VIRTUALENV=`which virtualenv-2.7`
-if [ "$VIRTUALENV" == "" ]; then
-    VIRTUALENV=`which virtualenv-2.6`
-fi
-if [ "$VIRTUALENV" == "" ]; then
-    VIRTUALENV=`which virtualenv`
-fi
-if [ "$VIRTUALENV" == "" ]; then
-    echo "Missing virtualenv!"
-    exit 1
-fi
-
 if [ ! -e env ]; then 
+    VIRTUALENV=`which virtualenv-2.7`
+    if [ "$VIRTUALENV" == "" ]; then
+        VIRTUALENV=`which virtualenv-2.6`
+    fi
+    if [ "$VIRTUALENV" == "" ]; then
+        VIRTUALENV=`which virtualenv`
+    fi
+    if [ "$VIRTUALENV" == "" ]; then
+        echo "Missing virtualenv!"
+        exit 1
+    fi
+
     echo "Initializing virtualenv folder (env)"
     $VIRTUALENV env
 fi
