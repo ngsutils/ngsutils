fc461f828e528470087c6c6da638851eb8186b41
Marcus Breese <marcus@breese.com>
2012-08-02 12:46:01 -0700

    use gcc to compile cython/pysam on Mac OS X Mountain Lion

--- init.sh
+++ init.sh
@@ -19,5 +19,15 @@ fi
 
 . env/bin/activate
 
+if [ $(uname -s) == 'Darwin' ]; then
+    # Mac OS X Mountain Lion compiles with clang by default...
+    # clang and cython don't get along...
+    # uname -sr = Darwin 12.0.0
+    if [ $(uname -r | sed -e 's/\./ /' | awk '{print $1}') == "12" ]; then
+        export CC=/usr/bin/gcc
+        export CXX=/usr/bin/g++
+    fi
+fi
+
 echo "Installing required libraries"
 pip install -r requirements.txt
