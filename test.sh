53560c05889d8c6865f8d742e7e9656527459974
Marcus Breese <marcus@breese.com>
2012-10-26 01:51:23 -0700

    added code to setup virtualenv and set pythonpath

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+REAL=`python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "$0"`
+DIR=`dirname "$REAL"`
+SUBDIR=$(basename $0 | sed -e 's/utils//')
+
+. "$DIR"/env/bin/activate
+export PYTHONPATH=$PYTHONPATH:"$DIR"
 
 if [ "$1" == "" ]; then
     # run all tests...
+   	cd $DIR
     python -m unittest discover ngsutils 'test*py' -v
 else
-    PYTHONPATH=. coverage run $1
+    coverage run $1
     if [ $? -eq 0 ]; then
         coverage report
     fi
