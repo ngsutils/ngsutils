831732ea9eda01d271e3ccfb92f10bb13e000c71
Marcus Breese <marcus@breese.com>
2012-10-26 17:34:59 -0700

    added tobedgraph test - NOTE: uses pileup output

---
+++ ngsutils/bam/t/test_tobedgraph.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''
+Tests for bamutils tag
+'''
+
+import os
+import unittest
+
+import ngsutils.bam
+import ngsutils.bam.tobedgraph
+import StringIO
+
+
+class ToBEDTest(unittest.TestCase):
+    def setUp(self):
+        self.bam = ngsutils.bam.bam_open(os.path.join(os.path.dirname(__file__), 'test.bam'))
+
+    def tearDown(self):
+        self.bam.close()
+
+    def testBEDGraph(self):
+        sio = StringIO.StringIO("")
+
+        ngsutils.bam.tobedgraph.bam_tobedgraph(self.bam, out=sio)
+        self.assertEqual(sio.getvalue(), '''\
+chr1\t99\t149\t1
+chr1\t174\t724\t2
+chr1\t724\t774\t1
+''')
+        sio.close()
+
+    def testBEDGraphPlus(self):
+        sio = StringIO.StringIO("")
+
+        ngsutils.bam.tobedgraph.bam_tobedgraph(self.bam, strand='+', out=sio)
+        self.assertEqual(sio.getvalue(), '''\
+chr1\t99\t149\t1
+chr1\t174\t724\t2
+''')
+        sio.close()
+
+    def testBEDGraphMinus(self):
+        sio = StringIO.StringIO("")
+
+        ngsutils.bam.tobedgraph.bam_tobedgraph(self.bam, strand='-', out=sio)
+        self.assertEqual(sio.getvalue(), '''\
+chr1\t724\t774\t1
+''')
+        sio.close()
+
+if __name__ == '__main__':
+    unittest.main()
