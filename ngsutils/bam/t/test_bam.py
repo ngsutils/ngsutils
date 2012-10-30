6104324284dbb272869545601304e590863fc128
Marcus Breese <marcus@breese.com>
2012-10-30 03:11:56 -0700

    added to run doctests

---
+++ ngsutils/bam/t/test_bam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+Tests for bamutils tag
+'''
+
+import unittest
+import doctest
+
+import ngsutils.bam
+import ngsutils.bam.convertregion
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(ngsutils.bam))
+    tests.addTests(doctest.DocTestSuite(ngsutils.bam.convertregion))
+    return tests
+
+if __name__ == '__main__':
+    unittest.main()
