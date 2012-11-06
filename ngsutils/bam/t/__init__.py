797fe5d5d6e6d0c1d68cd0f5abfd66b9e6eb2688
Marcus Breese <marcus@breese.com>
2012-11-05 16:04:42 -0800

    added fetch to mockbam to store and retrieved reads

--- ngsutils/bam/t/__init__.py
+++ ngsutils/bam/t/__init__.py
@@ -6,21 +6,75 @@ class MockBam(object):
     def __init__(self, refs):
         self._refs = {}
         for i, val in enumerate(refs):
-            self._refs[i + 1] = val
+            self._refs[i] = val
+        self._reads = {}
+        self._read_keys = []
+
+    def fetch(self, ref=None, start=-1, end=-1, region=None):
+        if region:
+            print "Unsupported option!"
+            sys.exit(1)
+
+        tid = -1
+
+        if ref and start > -1 and end > -1:
+            for i, val in enumerate(self._refs):
+                if val == ref:
+                    tid = i
+                    break
+            if tid == -1:
+                raise ValueError("Missing reference: %s" % ref)
+
+        for k in self._read_keys:
+            read = self._reads[k]
+            if tid == -1:
+                yield read
+            else:
+                if self._reads[k].tid == tid:
+                    if start <= read.pos <= end or start <= read.aend <= end or (read.pos < start and reads.aend > end):
+                        yield read
 
     def getrname(self, tid):
         return self._refs[tid]
 
+    def close(self):
+        pass
+
 
 class MockRead(object):
-    def __init__(self, qname, seq=None, qual=None, tid=-1, pos=-1, aend=None, is_reverse=False, cigar=None, tags=None):
+    def __init__(self, qname, seq=None, qual=None, tid=-1, pos=-1, aend=None, cigar=None, tags=None, is_reverse=False, is_paired=False, is_mate_unmapped=False, is_secondary=False, is_qcfail=False, flags=0):
         self.qname = qname
         self.seq = seq
         self.qual = qual
         self.tid = tid
         self.pos = pos
         self.aend = aend
+
+        self.is_paired = is_paired
+        self.is_mate_unmapped = is_mate_unmapped
         self.is_reverse = is_reverse
+        self.is_secondary = is_secondary
+        self.is_qcfail = is_qcfail
+
+        if flags:
+            self._flags = flags
+            self.is_paired = 0x1 & self.flags > 0
+            self.is_unmapped = 0x4 & self.flags > 0
+            self.is_mate_unmapped = 0x8 & self.flags > 0
+            self.is_reverse = 0x10 & self.flags > 0
+            self.is_secondary = 0x100 & self.flags > 0
+            self.is_qcfail = 0x200 & self.flags > 0
+        else:
+            self._flags = 0
+            self._flags |= 0x1 if self.is_paired else 0
+            self._flags |= 0x4 if self.is_unmapped else 0
+            self._flags |= 0x8 if self.is_mate_unmapped else 0
+            self._flags |= 0x10 if self.is_reverse else 0
+            self._flags |= 0x100 if self.is_secondary else 0
+            self._flags |= 0x200 if self.is_qcfail else 0
+
+        if self.tid == -1 or self.pos == -1:
+            self.is_unmapped = True
 
         if cigar:
             self.cigar = ngsutils.bam.cigar_fromstr(cigar)
@@ -39,6 +93,10 @@ class MockRead(object):
         return None
 
     @property
+    def flags(self):
+        return self._flags
+
+    @property
     def is_unmapped(self):
         if self.tid and self.pos > -1:
             return False
@@ -49,6 +107,8 @@ class MockRead(object):
         if val:
             self.tid = -1
             self.pos = -1
+            self._flags |= 0x4
         else:
             self.tid = 1
             self.pos = 0
+            self._flags = self._flags & 0xFFFB  # removes 0x4
