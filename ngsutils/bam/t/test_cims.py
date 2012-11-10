1ee8681f32afc269449fcf0e6601b5d78b5d0b9e
Marcus Breese <marcus@breese.com>
2012-11-09 16:40:00 -0800

    added placeholder test for experimenal cims method

---
+++ ngsutils/bam/t/test_cims.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+Tests for bamutils cims
+'''
+
+import unittest
+
+
+class CIMSTest(unittest.TestCase):
+    def testCIMS1(self):
+        'MISSING TEST/EXPERIMENTAL'
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
