410db9fce2060d2a7b4af64fe13d190ed3d12a4c
Marcus Breese <marcus@breese.com>
2012-11-15 13:21:25 -0800

    adding some support tests

--- ngsutils/support/stats.py
+++ ngsutils/support/stats.py
@@ -72,7 +72,31 @@ def fisher_test(a, b, c, d):
     return p
 
 
+def median(vals):
+    '''
+    >>> median([1,2,3])
+    2
+    >>> median([1,2,3,4])
+    2.5
+    '''
+    vals.sort()
+
+    if len(vals) % 2 == 1:
+        return vals[len(vals) / 2]
+    else:
+        a = vals[(len(vals) / 2) - 1]
+        b = vals[(len(vals) / 2)]
+        return float(a + b) / 2
+
+
 def mean_stdev(l):
+    '''
+    >>> mean_stdev([1,2,2,2])
+    (1.75, 0.5)
+    >>> mean_stdev([2,2,2,2])
+    (2.0, 0.0)
+    '''
+
     acc = 0
     for el in l:
         acc += el
@@ -84,3 +108,8 @@ def mean_stdev(l):
 
     stdev = math.sqrt(float(acc) / (len(l) - 1))
     return (mean, stdev)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
