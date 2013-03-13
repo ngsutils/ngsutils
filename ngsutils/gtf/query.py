8445928fb8977b099aed1701d28a0104f4b92dd8
Marcus Breese <marcus@breese.com>
2013-03-13 15:33:17 -0700

    adding more output (for debugging gtf file)

--- ngsutils/gtf/query.py
+++ ngsutils/gtf/query.py
@@ -16,7 +16,13 @@ def usage(msg=None):
     if msg:
         print '%s\n' % msg
     print __doc__
-    print 'Usage: gtfutils query filename.gtf{.gz} chrom:start-end'
+    print '''Usage: gtfutils query {options} filename.gtf{.gz} chrom:start-end
+
+Options:
+    -transcripts    show transcripts
+    -exons          show exons
+    -regions        show reduced regions
+'''
     sys.exit(1)
 
 
@@ -27,10 +33,19 @@ def gtf_query(gtf, chrom, start, end, strand=None):
 if __name__ == '__main__':
     filename = None
     region = None
+    show_transcripts = False
+    show_exons = False
+    show_regions = False
 
     for arg in sys.argv[1:]:
         if arg == '-h':
             usage()
+        elif arg == '-regions':
+            show_regions = True
+        elif arg == '-transcripts':
+            show_transcripts = True
+        elif arg == '-exons':
+            show_exons = True
         elif not filename and os.path.exists(arg):
             filename = arg
         elif not region:
@@ -48,3 +63,13 @@ if __name__ == '__main__':
 
     for gene in gtf_query(gtf, chrom, start, end):
         print gene
+        if show_regions:
+            for i, s, e, const, names in gene.regions:
+                print '    region %s: %s-%s (%s)' % (i, s, e, 'const' if const else 'alt')
+
+        if show_transcripts:
+            for t in gene.transcripts:
+                print '    transcript: %s (%s-%s)' % (t.transcript_id, t.start, t.end)
+                if show_exons:
+                    for i, (s, e) in enumerate(t.exons):
+                        print '        exon %s: %s-%s' % (i, s, e)
