de3c64b2740909491b44a3f502a7d046b245be5a
Marcus Breese <marcus@breese.com>
2013-04-29 10:45:04 -0700

    typo

--- ngsutils/bam/split.py
+++ ngsutils/bam/split.py
@@ -56,9 +56,8 @@ def bam_split(infile, out_template, read_count=1000000, reference=False, quiet=F
                 fname = '%s.%s.bam' % (out_template, file_count)
 
             outfile = pysam.Samfile(fname, "wb", template=bamfile)
-            lastref = read.tif
-
         outfile.write(read)
+        lastref = read.tid
         count += 1
 
     bamfile.close()
