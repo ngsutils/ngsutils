c0499039a9f957546392f632577e7ec0d5dc28ee
Marcus Breese <mbreese@stanford.edu>
2013-07-12 21:58:14 -0700

    summary format

--- ngsutils/bam/innerdist.py
+++ ngsutils/bam/innerdist.py
@@ -89,7 +89,7 @@ def bam_innerdist(bam1, bam2, summaryout=None):
             dist = read1.pos - read2.aend
 
         if summaryout:
-            summaryout.write(str(dist))
+            summaryout.write('%s\n' % dist)
 
         if not dist in distances:
             distances[dist] = 1
