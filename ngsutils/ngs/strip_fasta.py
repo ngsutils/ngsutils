28e736dbcabed9871cc42be9e5f7fbc1058119e1
Marcus Breese <mbreese@stanford.edu>
2013-08-22 17:52:17 -0700

    more status info to tell which references were removed

--- ngsutils/ngs/strip_fasta.py
+++ ngsutils/ngs/strip_fasta.py
@@ -16,22 +16,27 @@ from eta import eta_open_iter
 def strip_fasta(fname, substr='_'):
 	good = False
 	name = ''
+	rmlist = []
 	def callback():
 		if good:
 			return name
 		else:
 			return '%s (removed)' % name
-			
+
 	for line in eta_open_iter(fname, callback=callback):
 		if line[0] == '>':
 			name = line[1:].strip().split(' ')[0]
 			if substr in name:
 				good = False
+				rmlist.append(name)
 			else:
 				good = True
 		if good:
 			sys.stdout.write(line)
 
+	sys.stderr.write('Removed:\n')
+	for n in rmlist:
+		sys.stderr.write('%s\n' % n)
 
 
 def usage(msg=None):
