db40c4dfcdfddd30d7e115de6c14493319e0e754
Marcus Breese <mbreese@stanford.edu>
2013-08-30 14:33:50 -0700

    prepping for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ngsutils',
+      version='0.5.1a',
+      description='NGSUtils - Various utilities for working with NGS data',
+      author='Marcus Breese',
+      author_email='mbreese@stanford.edu',
+      url='http://ngsutils.org',
+      packages=['ngsutils', 'ngsutils.bam', 'ngsutils.bed', 'ngsutils.fastq', 'ngsutils.gtf', 'ngsutils.support', 'ngsutils.ngs'],
+      scripts=['bin/ngsutils', 'bin/fastqutils', 'bin/bamutils', 'bin/bedutils', 'bin/gtfutils']
+     )
