02530ce2a01a03f625a4255ab98c148f2717496c
Marcus Breese <mbreese@stanford.edu>
2013-08-30 15:03:14 -0700

    trying pypi for distribution

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(name='ngsutils',
       author_email='mbreese@stanford.edu',
       url='http://ngsutils.org',
       packages=['ngsutils', 'ngsutils.bam', 'ngsutils.bed', 'ngsutils.fastq', 'ngsutils.gtf', 'ngsutils.support', 'ngsutils.ngs'],
-      scripts=['bin/ngsutils', 'bin/fastqutils', 'bin/bamutils', 'bin/bedutils', 'bin/gtfutils']
+      scripts=['bin/ngsutils', 'bin/fastqutils', 'bin/bamutils', 'bin/bedutils', 'bin/gtfutils'],
+      install_requires = ['cython==0.16', 'pysam>=0.4.1', 'coverage>=3.5.3', 'eta>=0.9', 'swalign>=0.2']
      )
