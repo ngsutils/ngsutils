e52ccfa5063440b7c32585d850449242de074c67
Marcus Breese <mbreese@stanford.edu>
2013-09-11 23:40:33 -0700

    added a bunch of extra regions, txs, tlxs, junctions, introns - confirmed with IGV import

--- ngsutils/gtf/tobed.py
+++ ngsutils/gtf/tobed.py
@@ -25,12 +25,62 @@ Usage: gtfutils tobed [type] filename.gtf{.gz}
 Where type is one of:
     -genes    The gene from start to end (including introns)
     -exons    Each annotated exon
-    -regions  Export constant / alternative regions (annotated splice regions)
+    -introns  Each annotated intron
+    -regions  Export constant / alternative regions (annotated spliced regions)
     -tss      Transcription start sites (unique)
+    -txs      Transcription stop sites (unique)
     -tlss     Translational start sites (unique start codons)
+    -tlxs     Translational stop sites (unique stop codons)
+    -junc_5   Splice junction 5' donor
+    -junc_3   Splice junction 3' acceptor
+
 
 '''
     sys.exit(1)
+
+
+def gtf_junc_5_tobed(gtf, out=sys.stdout):
+    for gene in gtf.genes:
+        sites = set()
+        for i, txscr in enumerate(gene.transcripts):
+            if gene.strand == '+':
+                for j, (start, end) in enumerate(txscr.exons):
+                    if j == len(txscr.exons) - 1:
+                        continue
+
+                    if not end in sites:
+                        out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, end, end + 1, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                        sites.add(end)
+            else:
+                for j, (start, end) in enumerate(txscr.exons):
+                    if j == 0:
+                        continue
+
+                    if not start in sites:
+                        out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, start - 1, start, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                        sites.add(end)
+
+
+def gtf_junc_3_tobed(gtf, out=sys.stdout):
+    for gene in gtf.genes:
+        sites = set()
+        for i, txscr in enumerate(gene.transcripts):
+            if gene.strand == '-':
+                for j, (start, end) in enumerate(txscr.exons):
+                    if j == len(txscr.exons) - 1:
+                        continue
+
+                    if not end in sites:
+                        out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, end, end + 1, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                        sites.add(end)
+            else:
+                for j, (start, end) in enumerate(txscr.exons):
+                    if j == 0:
+                        continue
+
+                    if not start in sites:
+                        out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, start - 1, start, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                        sites.add(end)
 
 
 def gtf_genes_tobed(gtf, out=sys.stdout):
@@ -52,13 +102,41 @@ def gtf_tss_tobed(gtf, out=sys.stdout):
                     sites.add(txscr.end)
 
 
+def gtf_txs_tobed(gtf, out=sys.stdout):
+    for gene in gtf.genes:
+        sites = set()
+        for i, txscr in enumerate(gene.transcripts):
+            if gene.strand == '-':
+                if not txscr.start in sites:
+                    out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, txscr.start, txscr.start + 3, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                    sites.add(txscr.start)
+            else:
+                if not txscr.end in sites:
+                    out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, txscr.end - 3, txscr.end, '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                    sites.add(txscr.end)
+
+
 def gtf_tlss_tobed(gtf, out=sys.stdout):
     'Outputs all exons (from all transcripts)'
     for gene in gtf.genes:
         sites = set()
         for i, txscr in enumerate(gene.transcripts):
+            if not txscr.has_cds:
+                continue
             if not txscr.start_codon in sites:
                 out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, txscr.start_codon[0], txscr.start_codon[1], '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
+                sites.add(txscr.start_codon)
+
+
+def gtf_tlxs_tobed(gtf, out=sys.stdout):
+    'Outputs all exons (from all transcripts)'
+    for gene in gtf.genes:
+        sites = set()
+        for i, txscr in enumerate(gene.transcripts):
+            if not txscr.has_cds:
+                continue
+            if not txscr.stop_codon in sites:
+                out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, txscr.stop_codon[0], txscr.stop_codon[1], '%s.%s' % (gene.gene_name, i), 0, gene.strand]]))
                 sites.add(txscr.start_codon)
 
 
@@ -70,7 +148,23 @@ def gtf_exons_tobed(gtf, out=sys.stdout):
             exons.update(txscr.exons)
 
         for i, (start, end) in enumerate(sorted(exons)):
-            out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, start, end, '%s.%s' % (gene.gene_name, i + 1), 0, gene.strand]]))
+            out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, start, end, '%s.e%s' % (gene.gene_name, i + 1), 0, gene.strand]]))
+
+
+def gtf_introns_tobed(gtf, out=sys.stdout):
+    'Outputs all introns (from all transcripts)'
+    for gene in gtf.genes:
+        introns = set()
+        
+        for txscr in gene.transcripts:
+            last = None
+            for start, end in txscr.exons:
+                if last:
+                    introns.add((last, start))
+                last = end
+
+        for i, (start, end) in enumerate(sorted(introns)):
+            out.write('%s\n' % '\t'.join([str(x) for x in [gene.chrom, start, end, '%s.i.%s' % (gene.gene_name, i + 1), 0, gene.strand]]))
 
 
 def gtf_regions_tobed(gtf, out=sys.stdout):
@@ -86,9 +180,14 @@ def gtf_regions_tobed(gtf, out=sys.stdout):
 if __name__ == '__main__':
     genes = False
     exons = False
+    introns = False
     regions = False
     tss = False
     tlss = False
+    txs = False
+    tlxs = False
+    junc_5 = False
+    junc_3 = False
     filename = None
 
     for arg in sys.argv[1:]:
@@ -98,17 +197,27 @@ if __name__ == '__main__':
             genes = True
         elif arg == '-exons':
             exons = True
+        elif arg == '-introns':
+            introns = True
         elif arg == '-regions':
             regions = True
         elif arg == '-tss':
             tss = True
         elif arg == '-tlss':
             tlss = True
+        elif arg == '-txs':
+            txs = True
+        elif arg == '-tlxs':
+            tlxs = True
+        elif arg == '-junc_5':
+            junc_5 = True
+        elif arg == '-junc_3':
+            junc_3 = True
         elif not filename and os.path.exists(arg):
             filename = arg
 
     i = 0
-    for arg in [genes, exons, regions, tss, tlss]:
+    for arg in [genes, exons, introns, regions, tss, tlss, txs, tlxs, junc_5, junc_3]:
         if arg:
             i += 1
 
@@ -125,9 +234,19 @@ if __name__ == '__main__':
         gtf_genes_tobed(gtf)
     elif exons:
         gtf_exons_tobed(gtf)
+    elif introns:
+        gtf_introns_tobed(gtf)
     elif regions:
         gtf_regions_tobed(gtf)
     elif tss:
         gtf_tss_tobed(gtf)
     elif tlss:
         gtf_tlss_tobed(gtf)
+    elif txs:
+        gtf_txs_tobed(gtf)
+    elif tlxs:
+        gtf_tlxs_tobed(gtf)
+    elif junc_5:
+        gtf_junc_5_tobed(gtf)
+    elif junc_3:
+        gtf_junc_3_tobed(gtf)
