cfb95c1f2ca4119b8e9983f16ec6d31d4a9af965
Marcus Breese <mbreese@stanford.edu>
2013-10-02 17:02:40 -0700

    added "clean" method to convert float scores to integers

---
+++ ngsutils/bed/clean.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+## category General
+## desc Cleans a BED file (score should be integers)
+'''
+Cleanup a BED file, rounds scores to integers
+'''
+
+import os
+import sys
+from ngsutils.bed import BedFile, BedRegion
+
+
+def usage():
+    print __doc__
+    print """\
+Usage: bedutils clean bedfile
+
+Converts the "score" field to be an integer
+
+"""
+    sys.exit(1)
+
+
+def bed_clean(bed, out=sys.stdout):
+    for region in bed:
+        score = int(region.score)
+
+        out.write('%s\n' % BedRegion(region.chrom, region.start, region.end, region.name, score, region.strand))
+
+if __name__ == '__main__':
+    fname = None
+
+    for arg in sys.argv[1:]:
+        if arg == '-h':
+            usage()
+        if not fname and os.path.exists(arg):
+            fname = arg
+        else:
+            print "Unknown option: %s" % arg
+            usage()
+
+    if not fname:
+        usage()
+
+    bed_clean(BedFile(fname))
