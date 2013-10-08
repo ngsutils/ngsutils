c90323112310c09cc10e246ed3089c68c740d33d
Marcus Breese <mbreese@stanford.edu>
2013-10-08 14:47:12 -0700

    Added new program to calculate characteristics of peaks (BED) in a BAM file.

---
+++ ngsutils/bam/peakheight.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+## category General
+## desc Find the size (max height, width) of given peaks (BED) in a BAM file
+'''
+Find the size of a set of defined peaks (BED) in a BED file.
+
+If you have a set of defined peak regions (in the form of a BED file), this
+will return the size characteristics of the region for a given BAM file.
+
+The following fields will be added to the BED file:
+    maximum peak size
+    total unique reads
+    total coverage (sum of coverage over each base)
+    coverage density (total coverage / length)
+'''
+import sys
+import os
+from ngsutils.bam import bam_open
+
+
+def bam_peakheight(bam, bed_fobj, mask=1796):
+    for line in bed_fobj:
+        cols = line.strip('\n').split('\t')
+        coverage = []
+        reads = set()
+        ref = cols[0]
+        start = int(cols[1])
+        end = int(cols[2])
+        for pileup in bam.pileup(reference=ref, start=start, end=end, mask=mask):
+            coverage.append(pileup.n)
+            for read in pileup.pileups:
+                reads.add(read.alignment.qname)
+
+        cols.append(max(coverage))
+        cols.append(len(reads))
+        cols.append(sum(coverage))
+        cols.append(float(sum(coverage)) / (end-start))
+        print '\t'.join([str(x) for x in cols])
+
+
+def usage():
+    print __doc__
+    print """\
+Usage: bamutils peakheight {options} bamfile peaks.bed
+"""
+    sys.exit(1)
+
+if __name__ == "__main__":
+    bam_fname = None
+    bed_fname = None
+
+    for arg in sys.argv[1:]:
+        if arg == '-h':
+            usage()
+        elif not bam_fname and os.path.exists(arg):
+            bam_fname = arg
+        elif not bed_fname and os.path.exists(arg):
+            bed_fname = arg
+        else:
+            print 'Unknown argument: %s' % arg
+            usage()
+    if not bam_fname or not bed_fname:
+        usage()
+
+    bam = bam_open(bam_fname)
+    with open(bed_fname) as f:
+        bam_peakheight(bam, f)
+
+    bam.close()
