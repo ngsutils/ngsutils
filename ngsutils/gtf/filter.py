363e0d82c19afcca1554a45b04bf92413fb3b8f4
Marcus Breese <mbreese@stanford.edu>
2013-10-10 12:00:05 -0700

    added filter to remove annotation lines by reference chrom

---
+++ ngsutils/gtf/filter.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+## category General
+## desc Filter annotations from a GTF file
+'''
+Filter annotations from a GTF file.
+'''
+
+import sys
+import os
+
+from ngsutils.support import gzip_reader
+
+
+def usage():
+    print __doc__
+    print '''\
+Usage: gtfutils filter {filters} filename.gtf
+
+Possible filters:
+    -ref str    Remove annotations from chromosomes with 'str' in the name
+
+'''
+    sys.exit(1)
+
+
+def gtf_filter(fname, filters, out=sys.stdout):
+    for line in gzip_reader(fname):
+        cols = line.strip('\n').split('\t')
+        good = True
+        for filt in filters:
+            if not filt.process(cols):
+                good = False
+                break
+
+        if good:
+            out.write('%s\n' % '\t'.join([str(x) for x in cols]))
+
+
+class GTFFilter(object):
+    def process(self, cols):
+        raise NotImplementedError
+
+
+class RefSubstr(GTFFilter):
+    def __init__(self, substr):
+        self.substr = substr
+
+    def process(self, cols):
+        if self.substr in cols[0]:
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    fname = None
+    last = None
+    filters = []
+    for arg in sys.argv[1:]:
+        if arg == '-h':
+            usage()
+        elif last == '-ref':
+            filters.append(RefSubstr(arg))
+            last = None
+        elif arg in ['-ref']:
+            last = arg
+        elif not fname and os.path.exists(arg):
+            fname = arg
+        else:
+            usage()
+
+    if not fname or not filters:
+        usage()
+
+
+
+    gtf_filter(fname, filters)
