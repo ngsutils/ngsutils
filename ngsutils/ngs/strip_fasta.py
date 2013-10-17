4bc3e1cd5454433be7c922204a41c0fb0eb7e3f9
Marcus Breese <mbreese@stanford.edu>
2013-10-17 11:39:36 -0700

    whitespace

--- ngsutils/ngs/strip_fasta.py
+++ ngsutils/ngs/strip_fasta.py
@@ -14,52 +14,52 @@ import os
 from eta import eta_open_iter
 
 def strip_fasta(fname, substr='_'):
-	good = False
-	name = ''
-	rmlist = []
-	def callback():
-		if good:
-			return '%s (removed %s)' % (name, len(rmlist))
-		else:
-			return '%s [x] (removed %s)' % (name, len(rmlist))
+    good = False
+    name = ''
+    rmlist = []
+    def callback():
+        if good:
+            return '%s (removed %s)' % (name, len(rmlist))
+        else:
+            return '%s [x] (removed %s)' % (name, len(rmlist))
 
-	for line in eta_open_iter(fname, callback=callback):
-		if line[0] == '>':
-			name = line[1:].strip().split(' ')[0]
-			if substr in name:
-				good = False
-				rmlist.append(name)
-			else:
-				good = True
-		if good:
-			sys.stdout.write(line)
+    for line in eta_open_iter(fname, callback=callback):
+        if line[0] == '>':
+            name = line[1:].strip().split(' ')[0]
+            if substr in name:
+                good = False
+                rmlist.append(name)
+            else:
+                good = True
+        if good:
+            sys.stdout.write(line)
 
-	sys.stderr.write('Removed:\n')
-	for n in rmlist:
-		sys.stderr.write('%s\n' % n)
+    sys.stderr.write('Removed:\n')
+    for n in rmlist:
+        sys.stderr.write('%s\n' % n)
 
 
 def usage(msg=None):
-	if msg:
-		sys.stdout.write('%s\n' % msg)
-	sys.stdout.write(__doc__)
-	sys.stdout.write('Usage: ngsutils strip_fasta filename.fa [string-to-search-for]\n\n')
-	sys.exit(1)
+    if msg:
+        sys.stdout.write('%s\n' % msg)
+    sys.stdout.write(__doc__)
+    sys.stdout.write('Usage: ngsutils strip_fasta filename.fa [string-to-search-for]\n\n')
+    sys.exit(1)
 
 
 if __name__ == '__main__':
-	fname = None
-	substr = '_'
-	for arg in sys.argv[1:]:
-		if not fname:
-			if os.path.exists(arg):
-				fname = arg
-			else:
-				usage('%s missing!' % arg)
-		else:
-			substr = arg
+    fname = None
+    substr = '_'
+    for arg in sys.argv[1:]:
+        if not fname:
+            if os.path.exists(arg):
+                fname = arg
+            else:
+                usage('%s missing!' % arg)
+        else:
+            substr = arg
 
-	if not fname:
-		usage()
+    if not fname:
+        usage()
 
-	strip_fasta(fname, substr)
+    strip_fasta(fname, substr)
