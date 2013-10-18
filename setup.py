36b3e92cf17b245ec29b1181ecd8ca75101084bb
Marcus Breese <mbreese@stanford.edu>
2013-10-18 16:03:33 -0700

    requirement update

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(name='ngsutils',
       url='http://ngsutils.org',
       packages=['ngsutils', 'ngsutils.bam', 'ngsutils.bed', 'ngsutils.fastq', 'ngsutils.gtf', 'ngsutils.support', 'ngsutils.ngs'],
 #      scripts=['bin/ngsutils', 'bin/fastqutils', 'bin/bamutils', 'bin/bedutils', 'bin/gtfutils'],
-      install_requires = ['cython==0.16', 'pysam>=0.4.1', 'coverage>=3.5.3', 'eta>=0.9', 'swalign>=0.2']
+      install_requires = ['pysam>=0.4.1', 'coverage>=3.5.3', 'eta>=0.9', 'swalign>=0.2']
      )
