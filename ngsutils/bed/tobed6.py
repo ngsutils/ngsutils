6ff08e340deb956bd8e21fd0ead13aa25a273c03
Marcus Breese <mbreese@stanford.edu>
2013-10-30 15:17:54 -0700

    added tobed6

---
+++ ngsutils/bed/tobed6.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+## category Conversion
+## desc Removes extra columns from a BED (or BED compatible) file
+'''
+Removes extra columns from a BED (or BED compatible) file.
+
+'''
+
+import os
+import sys
+
+
+def usage():
+    print __doc__
+    print """\
+Usage: bedutils tobed6 bedfile
+
+"""
+    sys.exit(1)
+
+
+def bed_tobed6(fname, out=sys.stdout):
+    with open(fname) as f:
+        for line in f:
+            cols = line.strip().split('\t')
+            out.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5]))
+
+
+if __name__ == '__main__':
+    fname = None
+
+    for arg in sys.argv[1:]:
+        if arg == '-h':
+            usage()
+        elif not fname and os.path.exists(arg):
+            fname = arg
+        else:
+            print "Unknown option: %s" % arg
+            usage()
+
+    if not fname:
+        usage()
+
+    bed_tobed6(fname)
