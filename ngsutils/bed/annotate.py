7c351d533340fc8a2c6f062c73cc37c0135abee6
Marcus Breese <mbreese@stanford.edu>
2013-10-30 17:22:20 -0700

    doc

--- ngsutils/bed/annotate.py
+++ ngsutils/bed/annotate.py
@@ -5,6 +5,9 @@
 Convert a BED3 file to a BED6 file with constant name, score, and strand.
 This can also be used to replace values in a BED6 file, or add RGB colors based 
 on the "name" field of a region.
+
+If you add an RGB column, then the thickStart and thickEnd columns will also be
+added and set to the region start and end respectfully (if they are missing).
 '''
 
 import os
