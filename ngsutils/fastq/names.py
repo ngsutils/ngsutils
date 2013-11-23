13622f89e1bd62c75a8bc14901a1b79fdef6c116
Marcus Breese <mbreese@stanford.edu>
2013-11-22 16:23:38 -0800

    added ability to export comments too...

--- ngsutils/fastq/names.py
+++ ngsutils/fastq/names.py
@@ -11,26 +11,32 @@ import sys
 from ngsutils.fastq import FASTQ
 
 
-def export_names(fastq, out=sys.stdout, quiet=False):
+def export_names(fastq, include_comment=False, out=sys.stdout, quiet=False):
     for read in fastq.fetch(quiet=quiet):
-        out.write('%s\n' % read.name)
+        if include_comment:
+            out.write('%s%s%s\n' % (read.name, ' ' if read.comment else '', read.comment))
+        else:
+            out.write('%s\n' % read.name)
 
 
 def usage():
     print __doc__
-    print "Usage: fastqutils names filename.fastq{.gz}"
+    print "Usage: fastqutils names {-comment} filename.fastq{.gz}"
     sys.exit(1)
 
 if __name__ == '__main__':
     fname = None
+    include_comment = False
 
     for arg in sys.argv[1:]:
-        if os.path.exists(arg):
+        if arg == '-comment':
+            include_comment = True
+        elif os.path.exists(arg):
             fname = arg
 
     if not fname:
         usage()
 
     fq = FASTQ(fname)
-    export_names(fq)
+    export_names(fq, include_comment)
     fq.close()
