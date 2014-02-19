c4e21bfaf6767fbdcbfdb4145f911942d16c3116
Marcus Breese <mbreese@stanford.edu>
2014-02-19 10:45:22 -0800

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ngsutils',
-      version='0.5.3b',
+      version='0.5.4',
       description='NGSUtils - Various utilities for working with NGS data',
       author='Marcus Breese',
       author_email='mbreese@stanford.edu',
