616e75357edb2e2cf6b8d51342bb6a50d573f547
Marcus Breese <mbreese@stanford.edu>
2014-02-19 22:56:28 -0800

    added support for user-defined tmpdir

--- ngsutils/fastq/properpairs.py
+++ ngsutils/fastq/properpairs.py
@@ -21,20 +21,20 @@ from ngsutils.fastq import FASTQ
 import ngsutils.fastq.sort
 
 
-def find_fastq_pairs(fq1, fq2, out1, out2, quiet=False):
-    tmp1 = tempfile.NamedTemporaryFile(delete=False, prefix='.tmp', suffix='.gz', dir=os.path.dirname(fq1.fname))
+def find_fastq_pairs(fq1, fq2, out1, out2, tmpdir=None, quiet=False):
+    tmp1 = tempfile.NamedTemporaryFile(delete=False, prefix='.tmp', suffix='.gz', dir=tmpdir if tmpdir else os.path.dirname(fq1.fname))
     tmp1_fname = tmp1.name
     tmp1_out = gzip.GzipFile(fileobj=tmp1)
 
-    ngsutils.fastq.sort.fastq_sort(fq1, out=tmp1_out, tmpdir=os.path.dirname(fq1.fname))
+    ngsutils.fastq.sort.fastq_sort(fq1, out=tmp1_out, tmpdir=tmpdir if tmpdir else os.path.dirname(fq1.fname))
     tmp1_out.close()
     tmp1.close()
 
-    tmp2 = tempfile.NamedTemporaryFile(delete=False, prefix='.tmp', suffix='.gz', dir=os.path.dirname(fq2.fname))
+    tmp2 = tempfile.NamedTemporaryFile(delete=False, prefix='.tmp', suffix='.gz', dir=tmpdir if tmpdir else os.path.dirname(fq2.fname))
     tmp2_fname = tmp2.name
     tmp2_out = gzip.GzipFile(fileobj=tmp2)
 
-    ngsutils.fastq.sort.fastq_sort(fq2, out=tmp2_out, tmpdir=os.path.dirname(fq2.fname))
+    ngsutils.fastq.sort.fastq_sort(fq2, out=tmp2_out, tmpdir=tmpdir if tmpdir else os.path.dirname(fq2.fname))
     tmp2_out.close()
     tmp2.close()
 
@@ -92,8 +92,9 @@ def usage(msg=""):
     print """Usage: fastqutils properpairs filename1.fastq{.gz} filename2.fastq{.gz} output1 output2
 
 Options:
-  -f    Force overwriting output file (if it exists)
-  -z    Output files should be gzip compressed
+  -f       Force overwriting output file (if it exists)
+  -z       Output files should be gzip compressed
+  -t dir   Use {dir} for temporary files
 """
     sys.exit(1)
 
@@ -102,15 +103,27 @@ if __name__ == '__main__':
     fqname2 = None
     outname1 = None
     outname2 = None
+    tmpdir = None
     force = False
 
     gz = False
+
+    last = None
 
     for arg in sys.argv[1:]:
         if arg == '-z':
             gz = True
         elif arg == '-f':
             force = True
+        elif last == '-t':
+            if os.path.exists(arg) and os.path.isdir(arg):
+                tmpdir = arg
+            else:
+                usage('%s is not a valid temp-directory!' % arg)
+
+            last = None
+        elif arg in ['-t']:
+            last = arg
         elif not fqname1:
             if not os.path.exists(arg):
                 usage("File %s doesn't exist!" % arg)
@@ -142,7 +155,7 @@ if __name__ == '__main__':
         out1 = open(outname1, 'w')
         out2 = open(outname2, 'w')
 
-    paired, discard_1, discard_2 = find_fastq_pairs(fq1, fq2, out1, out2)
+    paired, discard_1, discard_2 = find_fastq_pairs(fq1, fq2, out1, out2, tmpdir)
 
     print "Proper pairs: %s" % paired
     print "Discarded 1 : %s" % discard_1
